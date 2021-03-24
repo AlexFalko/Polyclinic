e699fb8c81e4a1dbb7be4f90decf4ab258de689e
Alexander <sashafalko129@gmail.com>
2021-03-24 22:33:04 +0200

    images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_17_132740) do
+ActiveRecord::Schema.define(version: 2021_03_24_145610) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 2021_03_17_132740) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "type"
+    t.string "avatar"
     t.index ["category_id"], name: "index_users_on_category_id"
     t.index ["phone_number"], name: "index_users_on_phone_number", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
