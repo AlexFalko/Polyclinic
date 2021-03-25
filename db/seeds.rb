3aed7c9212ba4ab3e9818500598faea9a6c2ea32
Alexander <sashafalko129@gmail.com>
2021-03-25 23:52:47 +0200

    database credentials

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,6 @@ AdminUser.create!(phone_number: '+380686363616', password: 'password', password_
 end
 10.times do
   Patient.create(name: FFaker::Name.name,
-                 phone_number: FFaker::PhoneNumberUA.international_mobile_phone_number,
-                 password: 'password')
+                phone_number: FFaker::PhoneNumberUA.international_mobile_phone_number,
+                password: 'password')
 end
